0aae59518647b5bd41ce547343b2057b838803e2
jgj52 <email@jgj52.hu>
2026-03-28 21:45:53 +0100

    subcommand too

--- src/main/java/hu/jgj52/libCats/Types/SubCommand.java
+++ src/main/java/hu/jgj52/libCats/Types/SubCommand.java
@@ -2,6 +2,7 @@ package hu.jgj52.libCats.Types;
 
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -11,4 +12,9 @@ public abstract class SubCommand {
     public abstract String getName();
     public abstract boolean execute(@NotNull CommandSender sender, org.bukkit.command.@NotNull Command command, @NotNull String s, @NotNull String @NotNull [] args, @Nullable Player player);
     public abstract List<String> complete(@NotNull CommandSender sender, org.bukkit.command.@NotNull Command command, @NotNull String s, @NotNull String @NotNull [] args);
+
+    public String getMsg(String msg) {
+        return getPlugin().getConfig().getString("messages." + msg);
+    }
+    public abstract JavaPlugin getPlugin();
 }
