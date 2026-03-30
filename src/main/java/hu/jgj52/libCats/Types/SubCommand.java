ea19740ed4435d0113247d2293c521d8fe54a280
jgj52 <email@jgj52.hu>
2026-03-30 16:58:12 +0200

    i forgot subcommand

--- src/main/java/hu/jgj52/libCats/Types/SubCommand.java
+++ src/main/java/hu/jgj52/libCats/Types/SubCommand.java
@@ -1,11 +1,15 @@
 package hu.jgj52.libCats.Types;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class SubCommand {
@@ -16,6 +20,42 @@ public abstract class SubCommand {
 
     public String getMsg(String msg) {
         return getPlugin().getConfig().getString("messages." + msg);
+    }
+
+    public Component getComp(String msg) {
+        return MiniMessage.miniMessage().deserialize(getMsg(msg));
+    }
+
+    public Component getComp(String msg, boolean notItalic) {
+        Component component = getComp(msg);
+        if (!component.hasDecoration(TextDecoration.ITALIC) && notItalic) {
+            return component.decoration(TextDecoration.ITALIC, false);
+        }
+        return component;
+    }
+
+    public List<String> getMsgList(String msg) {
+        return getPlugin().getConfig().getStringList("messages." + msg);
+    }
+
+    public List<Component> getCompList(String msg) {
+        List<Component> list = new ArrayList<>();
+        for (String str : getMsgList(msg)) {
+            list.add(MiniMessage.miniMessage().deserialize(str));
+        }
+        return list;
+    }
+
+    public List<Component> getCompList(String msg, boolean notItalic) {
+        List<Component> components = getCompList(msg);
+        int i = 0;
+        for (Component component : components) {
+            if (!component.hasDecoration(TextDecoration.ITALIC) && notItalic) {
+                components.set(i, component.decoration(TextDecoration.ITALIC, false));
+            }
+            i++;
+        }
+        return components;
     }
     public abstract JavaPlugin getPlugin();
 }
