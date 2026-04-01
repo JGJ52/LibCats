95721f173bcefad3c6ada19453174778097fb70e
jgj52 <email@jgj52.hu>
2026-04-01 20:12:44 +0200

    this is 1.3.8

--- src/main/java/hu/jgj52/libCats/Types/Configuration.java
+++ src/main/java/hu/jgj52/libCats/Types/Configuration.java
@@ -1,6 +1,5 @@
 package hu.jgj52.libCats.Types;
 
-import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -9,6 +8,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public abstract class Configuration {
     private final File file;
@@ -18,27 +18,10 @@ public abstract class Configuration {
         reloadConfig();
         InputStream stream = getPlugin().getResource(getName() + ".yml");
         if (stream != null) {
-            YamlConfiguration defaults = YamlConfiguration.loadConfiguration(new InputStreamReader(stream));
-            YamlConfiguration already = YamlConfiguration.loadConfiguration(file);
-            remove(defaults, already);
-            getConfig().setDefaults(defaults);
+            getConfig().setDefaults(YamlConfiguration.loadConfiguration(new InputStreamReader(stream, StandardCharsets.UTF_8)));
         }
         getConfig().options().copyDefaults(true);
         saveConfig();
-    }
-
-    private void remove(ConfigurationSection defaults, ConfigurationSection already) {
-        for (String key : already.getKeys(false)) {
-            if (!already.isConfigurationSection(key)) {
-                defaults.set(key, null);
-            } else {
-                ConfigurationSection defaultSection = defaults.getConfigurationSection(key);
-                ConfigurationSection alreadySection = already.getConfigurationSection(key);
-                if (defaultSection != null && alreadySection != null) {
-                    remove(defaultSection, alreadySection);
-                }
-            }
-        }
     }
 
     public final FileConfiguration getConfig() {
